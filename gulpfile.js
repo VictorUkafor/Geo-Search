20b36dcb9c95b4a68fd9a92bcb064ab6691b7eb9
Victor Ukafor <victorukafor@gmail.com>
2019-11-11 15:38:50 +0100

    design the landing page
    - add styles for the header, body and footer
    - add styles for making the page beautiful for good UI/UX
    
    [Delivers issue#3]

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,15 @@ const uglify = require('gulp-uglify');
 const runSequence = require('run-sequence').use(gulp);
 const browserSync = require('browser-sync').create();
 
+
+// task for processing HTML files
 gulp.task('processHTML', done => {
     gulp.src('src/*.html')
     .pipe(gulp.dest('dist'));
     done();
 });
 
-
+// task for processing CSS files
 gulp.task('processCSS', done => {
     gulp.src('src/css/*.css')
     .pipe(uglify())
@@ -19,14 +21,14 @@ gulp.task('processCSS', done => {
     done();
 });
 
-
+// task for processing image files
 gulp.task('processImage', done => {
-    gulp.src('src/images/*.*')
-    .pipe(gulp.dest('dist/images'));
+    gulp.src('src/img/*.*')
+    .pipe(gulp.dest('dist/img'));
     done();
 });
 
-
+// task for processing JS files
 gulp.task('processJS', done => {
     gulp.src(['src/js/*.js'])
     .pipe(eslint())
@@ -39,24 +41,24 @@ gulp.task('processJS', done => {
   });
 
 
-  gulp.task('babelPolyfill', done => {
+gulp.task('babelPolyfill', done => {
     gulp.src('node_modules/babel-polyfill/browser.js')
     .pipe(gulp.dest('dist/node_modules/babel-polyfill'));
     done();
-  });  
+});  
 
-
- gulp.task('browserSync', done => {
+// task for browser for any changes
+gulp.task('browserSync', done => {
     browserSync.init({
       server: './dist',
       port: 8080,
       ui: { port: 8081 }
     });
     done();
-  }); 
+}); 
 
-
-  gulp.task('watch', ['browserSync'], () => {
+// task for watching for changes
+gulp.task('watch', ['browserSync'], () => {
     gulp.watch('src/js/*.js', ['processJS']);
     gulp.watch('src/*.html', ['processHTML']);
     gulp.watch('src/css/*.css', ['processCSS']);
@@ -64,9 +66,9 @@ gulp.task('processJS', done => {
     gulp.watch('dist/js/*.js', browserSync.reload);
     gulp.watch('dist/*.html', browserSync.reload);
     gulp.watch('dist/css/*.css', browserSync.reload);
-  });
+});
 
-  
+// task for running for all tasks concurrently  
 gulp.task('default', (callback) => {
   runSequence([
       'processHTML', 'processJS', 
